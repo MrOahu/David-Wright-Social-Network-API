e89ee9e096016620c3045f4c7d799c6001334852
David Wright <davidwright7793@gmail.com>
2024-04-30 19:02:59 -0400

    added user models

---
+++ models/User.js
@@ -0,0 +1,37 @@
+const { Schema, model } = require('mongoose');
+
+
+// Schema to create Student model
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+      match: [/^([a-z0-9_.-]+)@([\da-z.-]+).([a-z.]{2,6})$/, "needs to be a valid email address"]
+    },
+   thoughts: [{
+      type: Schema.Types.ObjectId,
+      ref: "thought"
+    }],
+   friends: [{
+    type: Schema.Types.ObjectId,
+    ref: "user"
+   }],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+  }
+);
+
+const User = model('user', userSchema);
+
+module.exports = User;
