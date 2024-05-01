4d38babe844de20c5d01288ff206c8376e7af5f8
David Wright <davidwright7793@gmail.com>
2024-05-01 14:14:54 -0400

    added schema to user.js

--- models/User.js
+++ models/User.js
@@ -28,9 +28,14 @@ const userSchema = new Schema(
   {
     toJSON: {
       getters: true,
+      virtuals: true
     },
   }
 );
+
+userSchema.virtual("friendCount").get(function(){
+  return this.friends.length
+})
 
 const User = model('user', userSchema);
 
